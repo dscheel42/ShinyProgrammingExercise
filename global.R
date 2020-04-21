003beeaa567049d5b0473eae12d6d08f75156f31
dscheel42 <danielscheel42@gmail.com>
2020-04-21 09:34:42 -0700

    Create trial data frame from two files
    This commit uses the two provided tsv files to create a dataframe that has a row for each patient observation that captures each of the three recorded values at that time.  It also provides patient characteristics alongside their observations and includes calculated fields for region and changes at the patient level between observations.
    
    This commit also establishes a new naming convention for the variables where each variable leads with a lowercase unless it is an abbrevation such as ALT.

--- global.R
+++ global.R
@@ -3,3 +3,75 @@ library(dplyr)
 library(ggplot2)
 library(forcats)
 library(data.table)
+
+setwd("~/Documents/GitHub/ShinyProgrammingExercise")
+PatientLevelInfo = fread("Random_PatientLevelInfo_2020.tsv", stringsAsFactors = T)
+LabValuesInfo = fread("Random_LabValuesInfo_2020.tsv", stringsAsFactors = T)
+
+#Cleaning Patient Level Info
+
+#Extract potential region identifier and patient id from USUBJID and put back into table
+
+splitFrame = tstrsplit(PatientLevelInfo$USUBJID,"-")
+
+PatientLevelInfo$region = paste(splitFrame[[2]],splitFrame[[3]], sep = '-')
+PatientLevelInfo$patientId = splitFrame[[5]]
+
+#Recode a treatment arm variable to be a single word describing treatment
+
+PatientLevelInfo$ACTARM = recode(PatientLevelInfo$ACTARM,
+                                 `A: Drug X` = "drugX",
+                                 `B: Placebo` = "placebo",
+                                 `C: Combination` = "combination"
+                                 )
+
+#Change the naming scheme of PatientLevelInfo to begin with lowercase and capitalize each subsequent concactenated word
+#USUBJID remains the same because we will use it join LabValuesInfo and PatientLevelInfo
+
+names(PatientLevelInfo) = c("studyId","USUBJID","age","sex","race","treatmentArm","actArmCd","region","patientId")
+
+#Drop redundant columns
+
+PatientLevelInfo = PatientLevelInfo %>%
+  select(-c("actArmCd"))
+
+#Before merging LabValuesInfo to the PatientLevelInfo we 
+#reconstruct it so that a single row captures the patient's 3 laboratory value measurements at a particular visit
+#calculate difference of measurements between visits for each patient
+#calculate the difference betwee the last recorded value week 5 and the baseline value
+#create a days from baseline variable
+#metadata such as the long name of ALT is excluded from the frame but displayed in the application
+
+PatientByVisit = LabValuesInfo %>% 
+  #make screening the first level of ALT
+  mutate(AVISIT = forcats::fct_relevel(AVISIT,"SCREENING")) %>% 
+  group_by(USUBJID,AVISIT) %>%
+  #Collect lab values by patient + visit 
+  summarise(ALT = AVAL[which(LBTESTCD == 'ALT')],
+            CRP = AVAL[which(LBTESTCD == 'CRP')],
+            IGA = AVAL[which(LBTESTCD == "IGA")],
+            BMRKR1 = unique(BMRKR1),
+            BMRKR2 = unique(BMRKR2)) %>%
+  #I want baseline to be 0 and screening to be negative (before the trial)
+  mutate(VISIT_NUM = as.numeric(AVISIT) - 2,
+         diffALT = c(NA,diff(ALT)),
+         diffCRP = c(NA,diff(CRP)),
+         diffIGA = c(NA,diff(IGA)),
+         #-1 is assigned to SCREENING as it occurs before the baseline record.
+         daysFromBaseline = recode(as.numeric(AVISIT),-1,0,7,14,21,28),
+         endDiffBaselineALT = ALT[which(VISIT_NUM == 5)] - ALT[which(VISIT_NUM == 0)],
+         endDiffBaselineCRP = CRP[which(VISIT_NUM == 5)] - CRP[which(VISIT_NUM == 0)],
+         endDiffBaselineIGA = IGA[which(VISIT_NUM == 5)] - IGA[which(VISIT_NUM == 0)]
+         ) %>%
+  select(-c("VISIT_NUM","AVISIT"))
+
+names(PatientByVisit) = c("USUBJID","ALT","CRP","IGA","biomarker1","biomarker2",
+                          "diffALT","diffCRP","diffIGA","daysFromBaseline",
+                          "endDiffBaselineALT","endDiffBaselineCRP","endDiffBaselineIGA")
+
+#Join together the PatientLevelInfo and PatientByVisit (formerly LabValuesInfo) frames
+
+TrialFrame = left_join(PatientLevelInfo,PatientByVisit,by = 'USUBJID')
+
+#Need to go back to following naming conventions on the race variable
+summary(TrialFrame$race)
